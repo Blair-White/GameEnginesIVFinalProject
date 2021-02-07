da22a75cf846de3348ccfa500f7e999a4e16280a
Blair-White <blairjwhite@hotmail.com>
2021-02-07 16:23:41 -0500

    added navmesh, skele logic idle->chasing->fighting, level changes.

--- FirstPersonRPG_FinalProject_GameEnginesIV/Assets/SkeletonController.cs
+++ FirstPersonRPG_FinalProject_GameEnginesIV/Assets/SkeletonController.cs
@@ -1,21 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SkeletonController : MonoBehaviour
 {
     public GameObject ModelObject, player;
-    private Animator animator;
+    public Animator animator;
     public float moveSpeed;
+    public NavMeshAgent agent;
     public enum States
     {
-        Idle, Chasing, Fighting, Damaged, Dying, Dead
+       EnterIdle, Idle, EnterChasing, Chasing, EnterFighting, Fighting, EnterDamaged, Damaged, EnterDying, Dying, EnterDead, Dead
     }
     public States State;
     // Start is called before the first frame update
     void Start()
     {
-        State = States.Idle;
+        State = States.EnterIdle;
         animator = ModelObject.GetComponent<Animator>();
         player = GameObject.Find("Player");
     }
@@ -25,12 +27,35 @@ public class SkeletonController : MonoBehaviour
     {
         switch (State)
         {
+            case States.EnterIdle:
+                animator.SetBool("isIdle", true);
+                State = States.Idle;
+                break;
             case States.Idle:
-                if(Vector3.Distance(transform.position, player.transform.position) < 10.0f)
-                { Debug.Log("Player in Range"); State = States.Chasing; }
+                if(Vector3.Distance(transform.position, player.transform.position) < 15.0f)
+                { Debug.Log("Player in Range"); State = States.EnterChasing; }
 
                 break;
+
+            case States.EnterChasing:
+                animator.SetBool("isChasing", true);
+                animator.SetBool("isIdle", false);
+                State = States.Chasing;
+                break;
             case States.Chasing:
+                if (Vector3.Distance(transform.position, player.transform.position) < 4.5f)
+                { State = States.EnterFighting; }
+                else
+                {
+                    agent.SetDestination(player.transform.position);
+                }
+                    break;
+            case States.EnterFighting:
+                animator.SetBool("isFighting", true);
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isChasing", false);
+                agent.isStopped = true;
+                State = States.Fighting;
                 break;
             case States.Fighting:
                 break;
